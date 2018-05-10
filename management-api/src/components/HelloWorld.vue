c15bea135e9de7fd972bbae367b69ec87c6bc6e0
gaku3601 <pro.gaku@gmail.com>
2018-05-10 19:16:26 +0900

    login画面を実装

--- management-api/src/components/HelloWorld.vue
+++ management-api/src/components/HelloWorld.vue
@@ -1,102 +1,30 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <i class="fas fa-check"></i>
-    <i class="fas fa-check"></i>
-    <i class="fas fa-dove"></i>
-    <div class="field">
+  <div class="login">
+    <h1>{{msg}}</h1>
+    <div class="form">
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Your Email">
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Password">
+        </div>
+      </div>
       <div class="control">
-        <input class="input is-primary" type="text" placeholder="Primary input">
+        <button class="button is-primary">Login</button>
       </div>
     </div>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Login',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to gApiG!!'
     }
   }
 }
@@ -104,18 +32,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.login{
+  margin-top:60px;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.form{
+  width: 300px;
+  margin: 0 auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button{
+  width: 100%;
 }
 </style>
