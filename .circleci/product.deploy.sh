f0de1ef77777c29b0d00f6f3dff0d4eb8651f464
gaku3601 <pro.gaku@gmail.com>
2018-05-12 13:35:36 +0900

    circleciの設定

---
+++ .circleci/product.deploy.sh
@@ -0,0 +1,10 @@
+docker login registry.gitlab.com -u ${UserName} -p ${Password}
+
+# コンテナのUPLOAD
+echo $CIRCLE_TAG
+ProjectName="gapig"
+ContainerName="management-api"
+docker build -t registry.gitlab.com/gaku3601/${ProjectName}/${ContainerName}:${CIRCLE_TAG} ./management-api/.
+docker tag registry.gitlab.com/gaku3601/${ProjectName}/${ContainerName}:${CIRCLE_TAG} registry.gitlab.com/gaku3601/${ProjectName}/${ContainerName}:latest
+docker push registry.gitlab.com/gaku3601/${ProjectName}/${ContainerName}:${CIRCLE_TAG}
+docker push registry.gitlab.com/gaku3601/${ProjectName}/${ContainerName}:latest
